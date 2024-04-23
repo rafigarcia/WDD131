4d9787e8afa0e7cc0816e1d18270583f4f6b138d
rafigarcia <rafaela.garcia.angulo@gmail.com>
2024-04-23 13:25:31 -0500

    Week01 Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,18 @@
+//Get the Dates and display last modified
+
+const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+}
+
+const todaysdate = new Date();
+const year = todaysdate.getFullYear();
+
+document.getElementById("year").textContent = year;
+
+const lastModifiedTimestamp = document.lastModified;
+const lastmodifiedDate = new Date(lastModifiedTimestamp);
+
+document.getElementById("lastModified").textContent = lastmodifiedDate.toLocaleDateString("en-US", options);
